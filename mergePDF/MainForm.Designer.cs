7658fef2a19546c615f49234a867e54d31b6ff2b
e1500844 <e1500844@edu.vamk.fi>
2018-12-17 19:15:28 +0200

    init

---
+++ mergePDF/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reviewButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reviewButton
+            // 
+            this.reviewButton.Location = new System.Drawing.Point(257, 355);
+            this.reviewButton.Name = "reviewButton";
+            this.reviewButton.Size = new System.Drawing.Size(75, 23);
+            this.reviewButton.TabIndex = 0;
+            this.reviewButton.Text = "Esikatsele";
+            this.reviewButton.UseVisualStyleBackColor = true;
+            this.reviewButton.Click += new System.EventHandler(this.ReviewButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(404, 355);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Tallenna";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.reviewButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reviewButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ mergePDF/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private string tempPath = Path.Combine(Path.GetTempPath(), "mergedtmpdocument.pdf");
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MergePDF()
+        {
+            using (PdfDocument one = PdfReader.Open(@"C:\Users\tomkoi\Documents\Oppari\abc.pdf", PdfDocumentOpenMode.Import))
+            using (PdfDocument two = PdfReader.Open(@"C:\Users\tomkoi\Documents\Oppari\UiPath Orchestrator 2018.1 Diploma.pdf", PdfDocumentOpenMode.Import))
+            using (PdfDocument outPdf = new PdfDocument())
+            {
+                CopyPages(one, outPdf);
+                CopyPages(two, outPdf);
+
+                outPdf.Save(tempPath);
+            }
+
+            void CopyPages(PdfDocument from, PdfDocument to)
+            {
+                for (int i = 0; i < from.PageCount; i++)
+                {
+                    to.AddPage(from.Pages[i]);
+                }
+            }
+        }
+
+        private void Review(string pathToPDF)
+        {
+            try
+            {
+                Process.Start(pathToPDF);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (File.Exists(tempPath))
+                File.Delete(tempPath);
+        }
+
+        private void ReviewButton_Click(object sender, EventArgs e)
+        {
+            Review(tempPath);
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            MergePDF();
+        }
+    }
+}
